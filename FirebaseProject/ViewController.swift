35c07f6daa8a01af17e7b1f4cc734f88f8d8d41a
Enesssk <140720772+Enesssk@users.noreply.github.com>
2024-03-05 12:32:04 +0300

    design is over , auth is enabled

--- FirebaseProject/ViewController.swift
+++ FirebaseProject/ViewController.swift
@@ -6,14 +6,74 @@
 //
 
 import UIKit
+import Firebase
+
+
+
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var emailText: UITextField!
+    
+    @IBOutlet weak var passwordText: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        
     }
+    
+    @IBAction func signInButtonClicked(_ sender: Any) {
+        
+        if emailText.text == "" && passwordText.text == "" {
+            self.makealert(titleInput: "Error", messageInput: "Please enter a email or password")
+        } else {
+            Auth.auth().signIn(withEmail: emailText.text!, password: passwordText.text!) { response, error in
+                
+                if error != nil {
+                    self.makealert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+                
+            }
+        }
+        
+    }
+    
+    
+    @IBAction func signUpButtonClicked(_ sender: Any) {
+        
+        if emailText.text == "" && passwordText.text == "" {
+            self.makealert(titleInput: "Error", messageInput: "Please enter a email or password")
+        } else {
+            Auth.auth().createUser(withEmail: emailText.text!, password: passwordText.text!) { result, error in
+                if error != nil {
+                    self.makealert(titleInput: "Error", messageInput: error?.localizedDescription ?? "Error")
+                } else {
+                    self.performSegue(withIdentifier: "toFeedVC", sender: nil)
+                }
+            }
 
-
+        }
+        
+        
+    }
+    func makealert (titleInput : String, messageInput : String) {
+        
+        let alert = UIAlertController(title: titleInput, message: messageInput, preferredStyle: UIAlertController.Style.alert)
+        let okButton = UIAlertAction(title: "Ok", style: UIAlertAction.Style.default, handler: nil)
+        alert.addAction(okButton)
+        
+        self.present(alert, animated: true)
+        
+    }
+    
+    
 }
+    
+
 
